4d2f80a43d1f0a63821b4c238b1e7c2e288df4c6
Diego22rct <diegorafaelcisnerostafur@gmail.com>
2025-10-15 09:48:31 -0500

    refactor: improve layout and styling of OptionCard component for better readability

--- components/option-card.tsx
+++ components/option-card.tsx
@@ -1,6 +1,6 @@
+import Feather from "@expo/vector-icons/Feather";
 import { JSX } from "react";
 import { Linking, Text, TouchableOpacity, View } from "react-native";
-
 
 type Props = {
   icon: JSX.Element;
@@ -9,17 +9,23 @@ type Props = {
   link: string;
 };
 
-export default function OptionCard({icon, title, description, link} : Props) {
+export default function OptionCard({ icon, title, description, link }: Props) {
   return (
-    <View className='bg-white rounded-lg shadow-md p-4 mb-4 w-4/5'>
-      <View className='flex-row items-center mb-2'>
-        {icon}
-        <Text className='text-lg font-bold'>{title}</Text>
+    <View className="flex-row items-start col-span-2 bg-white rounded-lg shadow-md p-4 mb-4 w-11/12">
+      <View className="flex-1 pr-4">
+        <View className="flex-row items-center mb-2">
+          {icon}
+          <Text className="text-lg font-bold ml-2">{title}</Text>
+        </View>
+        <Text className="text-gray-600">{description}</Text>
+        <TouchableOpacity onPress={() => Linking.openURL(link)} className="mt-2">
+          <Text className="text-blue-500">Learn more</Text>
+        </TouchableOpacity>
       </View>
-      <Text className='text-gray-600'>{description}</Text>
-      <TouchableOpacity onPress={() => Linking.openURL(link)} className='mt-2'>
-        <Text className='text-blue-500'>Learn more</Text>
-      </TouchableOpacity>
+
+      <View className="justify-center items-center">
+        <Feather name="chevron-right" size={24} color="black" />
+      </View>
     </View>
   );
 }
