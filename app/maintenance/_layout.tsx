13b2bbb45cd38637620f9e4939da685075f1c184
Diego22rct <diegorafaelcisnerostafur@gmail.com>
2025-10-16 11:25:53 -0500

    feat: add MaintenanceLayout component with navigation setup

---
+++ app/maintenance/_layout.tsx
@@ -0,0 +1,13 @@
+import { Stack } from 'expo-router';
+
+export default function MaintenanceLayout() {
+    return (
+        <Stack
+            screenOptions={{
+                headerShown: false,
+            }}
+        >
+            <Stack.Screen name="select-building" />
+        </Stack>
+    );
+}
